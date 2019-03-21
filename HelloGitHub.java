e2d8cd15566eb29fc749d4ab2ef98dcb886a29c7
samisyed720 <48797414+samisyed720@users.noreply.github.com>
2019-03-21 10:22:37 -0400

    initital Test
    just playing around with GitHub

---
+++ HelloGitHub.java
@@ -0,0 +1,21 @@
+/**
+ * Simplest Java Program 
+ * @course ICS4UC
+ * @Date 
+ **/
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ **/
+public class HelloGitHub
+{
+  // 
+  public static void main(String args[])
+  {
+    System.out.println("Hello GitHub");
+  }
+  
+}
